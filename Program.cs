a356a53a06619a7577961ee9c7b8e8e7f711fee9
Sherajul-Shakil <sherajulshakil14@cse.pstu.ac.bd>
2025-02-11 09:44:20 +0600

    Add swagger documentation

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,14 +12,51 @@ var builder = WebApplication.CreateBuilder(args);
 var connString = builder.Configuration.GetConnectionString("GameStore");
 builder.Services.AddSqlite<GameStoreContext>(connString);
 
+// Add Swagger services
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+	options.SwaggerDoc("v1", new OpenApiInfo
+	{
+		Title = "GameStore API",
+		Version = "v1",
+		Description = "A REST API for managing a game store",
+	});
+
+	// Configure JWT Authentication in Swagger
+	options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+	{
+		Name = "Authorization",
+		Type = SecuritySchemeType.Http,
+		Scheme = "Bearer",
+		BearerFormat = "JWT",
+		In = ParameterLocation.Header,
+		Description = "Enter 'Bearer {your_token_here}'"
+	});
+
+	options.AddSecurityRequirement(new OpenApiSecurityRequirement
+	{
+		{
+			new OpenApiSecurityScheme
+			{
+				Reference = new OpenApiReference
+				{
+					Type = ReferenceType.SecurityScheme,
+					Id = "Bearer"
+				}
+			},
+			Array.Empty<string>()
+		}
+	});
+});
+
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 	.AddJwtBearer(options =>
 	{
-		// Get secret key from appsettings.json
 		var secretKey = builder.Configuration["JwtSettings:SecretKey"];
 		if (string.IsNullOrEmpty(secretKey))
 		{
-			throw new Exception("JWT secret key is missing. Please configure the 'Jwt:Key' setting in appsettings.json.");
+			throw new Exception("JWT secret key is missing. Please configure the 'JwtSettings:SecretKey' setting in appsettings.json.");
 		}
 
 		options.TokenValidationParameters = new TokenValidationParameters
@@ -31,17 +69,30 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 	});
 
 builder.Services.AddAuthorization();
+
 var app = builder.Build();
+
+// Enable Swagger UI
+if (app.Environment.IsDevelopment())
+{
+	app.UseSwagger();
+	app.UseSwaggerUI(options =>
+	{
+		options.SwaggerEndpoint("/swagger/v1/swagger.json", "GameStore API v1");
+		options.RoutePrefix = string.Empty;
+		// options.RoutePrefix = "swagger";
+
+	});
+}
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapGamesEndpoints();
 app.MapGenresEndpoints();
 app.MapAuthEndpoints();
 app.MapExternalApiEndpoints();
-
-app.MapGet("/", () => "Hello, World!");
-
-app.UseAuthentication();
-app.UseAuthorization();
+// app.MapGet("/", () => "Hello, World!");
 
 await app.MigrateDbAsync();
 app.Run();
@@ -55,3 +106,4 @@ app.Run();
 // dotnet ef migrations add InitialCreate --output-dir Data\Migrations
 // dotnet ef database update
 // git rm -r --cached obj
+// http://localhost:5074/swagger/index.html
